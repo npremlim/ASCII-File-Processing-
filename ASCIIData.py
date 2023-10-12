69da522d7a1ecc8315a7b692b0cb6177d0768c0b
Naomi <59903147+npremlim@users.noreply.github.com>
2023-10-12 13:04:29 -0700

    Updates regarding export options
    1) Able to read multiple peak values of CO2
    2) able to import to excel

--- ASCIIData.py
+++ ASCIIData.py
@@ -9,50 +9,81 @@ internal = 0
 temp=[]
 datestore=""
 timestore=""
+co2= False
 output = open("output.txt", "w")
 filename="Filename= "+droppedFile
 title="Sample" +"    "  " CH4	      CO2	 N2O       Time	            Date"
-print(filename,file=output)
-print("\n",file=output)
+import pandas as pd
+df = pd.DataFrame(columns=['CH4', 'CO2', 'N2O', 'Time','Date'])
 
-print(title,file=output)
+print(filename,file=output)
+# print("\n",file=output)
+
+# print(title,file=output)
+co2peak=0
+start=False
 for line in lines:
-    if "[Header]" in line:
-        temp.append(str(timestore).ljust(15))
-        temp.append(str(datestore).ljust(15))
-        timestore=""
-        datestore=""
-        mySeparator = " "
-        insert = mySeparator.join(temp)
-        print(insert,file=output)
-        print(temp)
-        temp=[]
-        printing = False
-        gastype=0
-        temp.append(str(sampleno).ljust(10))
-        sampleno+=1
+    if "[Header]" in line and start:
+            temp.append(str(timestore).ljust(15))
+            temp.append(str(datestore).ljust(15))
+            timestore=""
+            datestore=""
+            mySeparator = " "
+            insert = mySeparator.join(temp)
+            # print(len(temp))
+            
+            df.loc[len(df)] = temp
+            # print(insert,file=output)
+            # print(temp,file=output)
+            #print(temp)
+            temp=[]
+            printing = False
+            gastype=0
+            # temp.append(str(sampleno).ljust(10))
+            sampleno+=1
         
  
     if "[Peak Table(Ch1)]" in line:
+        co2= False
         internal=0
         printing= True
 
     if "[Peak Table(Ch2)]" in line:
         internal=0
+        co2= True
     
     if "[Peak Table(Ch3)]" in line:
+        co2= False
         internal=0
         
 
     if printing :
+       
         if internal == 1:
+            peaks= line.split()
+            peakno = int(peaks[3])
+            if co2:
+                co2peak= int(peaks[3])
             if "0" in line:
                 temp.append(str(0).ljust(10))
+
+           
         
-        if internal==3:
+        if internal>=3 and peakno!=0:
             date= line.split()
             insertval= date[4]
-            temp.append(insertval.ljust(10))
+            if(co2):
+                if(float(date[1])<7.70 or float(date[1])>7.90):
+                    insertval="0"
+                    #print("co2 val is "+ date[1])
+                    
+            if co2:
+                if(co2peak==1):
+                    temp.append(insertval.ljust(10))
+                co2peak-=1
+            else:
+                temp.append(insertval.ljust(10))
+            peakno-=1
 
         internal+=1
 
@@ -71,12 +102,19 @@ for line in lines:
 
 
      
-    count+=1    
-    
+    count+=1 
+    start=True   
+
+
+# temp.append(str(timestore).ljust(15))
+# temp.append(str(datestore).ljust(15))
 temp.append(str(timestore).ljust(15))
 temp.append(str(datestore).ljust(15))
-insert = mySeparator.join(temp)
-print(temp)
-print(insert,file=output)
-    
+# insert = mySeparator.join(temp)
+df.loc[len(df)] = temp
+# print(temp)
+# print(insert,file=output)
+# print(temp,file=output)
+print(df.to_csv(), file=output)
+
 output.close()
